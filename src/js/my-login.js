86a7a63274d0ef79ab57bd3c4220109a81466e2d
Zachary-E-Hamilton <zachary.hamilton@maine.edu>
2023-10-23 00:29:23 -0400

    basic homepage

--- src/js/my-login.js
+++ src/js/my-login.js
@@ -78,11 +78,13 @@ $(function() {
 	});
 
 	$(".my-login-validation").submit(function() {
-		var form = $(this);
-        if (form[0].checkValidity() === false) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
-		form.addClass('was-validated');
+		event.preventDefault();
+		window.location.href="home.html"
+		// var form = $(this);
+        // if (form[0].checkValidity() === false) {
+        //   event.preventDefault();
+        //   event.stopPropagation();
+        // }
+		// form.addClass('was-validated');
 	});
 });
