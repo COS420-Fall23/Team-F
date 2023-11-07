5725690520c4a2435a78f2c748953c74ba71d3bf
LukeTompk <boxlan246@gmail.com>
2023-11-07 16:45:29 -0500

    Quiz test update 2
    Formatting fix

--- test/test-quiz.js
+++ test/test-quiz.js
@@ -1,24 +1,22 @@
-const quizFile = '../src/quiz.html';
 const { expect } = require('chai');
 const chai = require('chai');
 const { JSDOM } = require('jsdom');
 chai.use(require('chai-dom'));
 require('jsdom-global')();
 
-describe(quizFile, () => {
+describe('src/quiz.html', () => {
   beforeEach((done) => {
-   JSDOM.fromFile(quizFile)
+   JSDOM.fromFile('src/quiz.html')
    .then((dom) => {
      global.document = dom.window.document
    })
  .then(done, done);
- });
-})
-
-describe("Quiz page tests", () => {
-  it("Quiz page has title 'Self-Assessment Quiz'", () => {
-   let element = document.querySelector('.quiz-body').querySelector('h1')
-   expect(element).to.have.text("Self-Assessment Quiz")
-  })
  })
- 
+
+describe("Quiz tests", () => {
+ it("h1 heading should say 'Self-Assessment Quiz'", () => {
+  let element = document.querySelector('.quiz-body').querySelector('h1')
+  expect(element).to.have.text("Self-Assessment Quiz")
+ })
+})
+})
