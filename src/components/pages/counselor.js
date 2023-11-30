3e152d551458c076c052dd98b64a05b4dbdcf7fc
Shashank-RC <shashankreddy0402@gmail.com>
2023-11-29 21:55:49 -0500

    changing counselor.css to match color scheme

--- src/components/pages/counselor.js
+++ src/components/pages/counselor.js
@@ -26,31 +26,49 @@ class Counselors extends Component {
             className={activeUniversity === 'univ_a' ? 'active' : ''}
             onClick={() => this.setActiveUniversity('univ_a')}
           >
-            Counselors at University A
+            Counselors at University of Maine Orono
           </button>
           <button 
             id="btnUniversityB"
             className={activeUniversity === 'univ_b' ? 'active' : ''}
             onClick={() => this.setActiveUniversity('univ_b')}
           >
-            Counselors at University B
+            Counselors at University of Southern Maine
           </button>
 
-          {/* Counselors for University A */}
+          {/* Counselors for University of Maine Orono */}
           <div className={activeUniversity === 'univ_a' ? 'counselor' : 'counselor hidden'} data-university="univ_a">
             <h2>John Doe</h2>
-            <p><strong>Bio:</strong> Detailed biography of John Doe...</p>
-            <p><strong>Availability:</strong> Details...</p>
+            <p><strong>Bio:</strong> John Doe, an esteemed alumnus of the University of Maine Orono, has been a dedicated career counselor for over 10 years...</p>
+            <p><strong>Availability:</strong> Monday to Friday, 9 AM - 5 PM</p>
           </div>
-          {/* Add similar divs for other counselors of University A */}
+          <div className={activeUniversity === 'univ_a' ? 'counselor' : 'counselor hidden'} data-university="univ_a">
+            <h2>Jane Doe</h2>
+            <p><strong>Bio:</strong> Jane Doe holds a Master’s degree in Career Counseling and has been part of the University of Maine Orono team for over 7 years...</p>
+            <p><strong>Availability:</strong> Monday, Wednesday, Friday, 10 AM - 4 PM</p>
+          </div>
+          <div className={activeUniversity === 'univ_a' ? 'counselor' : 'counselor hidden'} data-university="univ_a">
+            <h2>Peter Glenn</h2>
+            <p><strong>Bio:</strong> Peter Glenn, with a background in international business, brings over 15 years of experience...</p>
+            <p><strong>Availability:</strong> Tuesday and Thursday, 8 AM - 6 PM</p>
+          </div>
 
-          {/* Counselors for University B */}
+          {/* Counselors for University of Southern Maine */}
           <div className={activeUniversity === 'univ_b' ? 'counselor' : 'counselor hidden'} data-university="univ_b">
             <h2>Alex Morgan</h2>
-            <p><strong>Bio:</strong> Detailed biography of Alex Morgan...</p>
-            <p><strong>Availability:</strong> Details...</p>
+            <p><strong>Bio:</strong> Alex Morgan, a career advisor with a specialty in environmental sciences, has been with the University of Southern Maine for over 8 years...</p>
+            <p><strong>Availability:</strong> Monday to Wednesday, 9 AM - 3 PM</p>
           </div>
-          {/* Add similar divs for other counselors of University B */}
+          <div className={activeUniversity === 'univ_b' ? 'counselor' : 'counselor hidden'} data-university="univ_b">
+            <h2>Rachel Lee</h2>
+            <p><strong>Bio:</strong> Rachel Lee brings a dynamic approach to career counseling, focusing on the arts and creative industries...</p>
+            <p><strong>Availability:</strong> Thursday and Friday, 11 AM - 5 PM</p>
+          </div>
+          <div className={activeUniversity === 'univ_b' ? 'counselor' : 'counselor hidden'} data-university="univ_b">
+            <h2>Michael Thompson</h2>
+            <p><strong>Bio:</strong> Michael Thompson, with his experience in international business and a Ph.D. in Business Administration, specializes in counseling students...</p>
+            <p><strong>Availability:</strong> Monday, Thursday, Friday, 10 AM - 4 PM</p>
+          </div>
         </div>
       </div>
     );
